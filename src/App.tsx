081e57d7a1de185a771509befd46af4f50cc0243
ARTEM <artemsmirnof@mail.ru>
2024-08-13 15:23:24 +0300

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { parseInitData, retrieveLaunchParams } from '@telegram-apps/sdk';
+import React from 'react';
+import { SDKProvider} from '@telegram-apps/sdk-react';
+import TappingPage from './components/TappingPage';
 
 const App: React.FC = () => {
-  const [userId, setUserId] = useState<string | null>(null);
-  const [consol, setConsol] = useState([]);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    try {
-      setConsol(...consol, "initData");
-      const initData  = retrieveLaunchParams();
-      setConsol(...consol, initData);
-      
-      if (initData) {
-        // Assuming initData is a JSON string or can be parsed directly
-        const urlParams = new URLSearchParams();
-        const userJson = urlParams.get('user');
-        
-        if (userJson) {
-          const user = JSON.parse(userJson);
-          setUserId(user.id);
-        } else {
-          throw new Error('User data not found in initData');
-        }
-      } else {
-        throw new Error('InitData is not defined');
-      }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (e) {
-      console.log("eeee");
-    }
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  // const [userId, setUserId] = useState<string | null>(null);
+  
 
   return (
-    <div>
-      User ID: {userId}
-      <br />
-      {consol}
-    </div>
+    <SDKProvider acceptCustomStyles >
+      <TappingPage/>
+    </SDKProvider>
   );
 };
 
--- src/components/TappingPage.tsx
+++ src/components/TappingPage.tsx
@@ -1,10 +1,16 @@
+import { useInitData } from "@telegram-apps/sdk-react";
 
 function TappingPage() {
-  
+  const initData = useInitData();
+
 
   return (
     <>
-     
+     <div>
+      {/* User ID: {userId} */}
+      <br />
+      {initData ? JSON.stringify(initData.user) : 1}
+    </div>
     </>
   )
 }
