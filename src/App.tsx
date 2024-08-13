c320c4688e5b230d318724dc163930aaf3e5d77d
ARTEM <artemsmirnof@mail.ru>
2024-08-13 15:02:16 +0300

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { retrieveLaunchParams } from '@telegram-apps/sdk';
+import { parseInitData, retrieveLaunchParams } from '@telegram-apps/sdk';
 
 const App: React.FC = () => {
   const [userId, setUserId] = useState<string | null>(null);
+  const [consol, setConsol] = useState([]);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     try {
-      const { initData } = retrieveLaunchParams();
+      setConsol(...consol, "initData");
+      const initData  = retrieveLaunchParams();
+      setConsol(...consol, initData);
       
       if (initData) {
         // Assuming initData is a JSON string or can be parsed directly
-        const urlParams = new URLSearchParams(initData);
+        const urlParams = new URLSearchParams();
         const userJson = urlParams.get('user');
         
         if (userJson) {
@@ -23,8 +26,9 @@ const App: React.FC = () => {
       } else {
         throw new Error('InitData is not defined');
       }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (e) {
-      setError(`Error retrieving or parsing launch parameters: ${e.message}`);
+      console.log("eeee");
     }
   }, []);
 
@@ -32,7 +36,13 @@ const App: React.FC = () => {
     return <div>Error: {error}</div>;
   }
 
-  return <div>User ID: {userId}</div>;
+  return (
+    <div>
+      User ID: {userId}
+      <br />
+      {consol}
+    </div>
+  );
 };
 
 export default App;
