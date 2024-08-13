4f791d0f40ce81b4a35f8d0ebe9b1fe83f34e7cc
ARTEM <artemsmirnof@mail.ru>
2024-08-13 13:13:51 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,32 @@ import { retrieveLaunchParams } from '@telegram-apps/sdk';
 function App() {
   const [userId, setUserId] = useState(0);
   useLayoutEffect(()=>{
-    const { initData } = retrieveLaunchParams();
-    // Проверка, что initData определен и имеет корректный формат
-    if (initData) {
-      try {
-        // Создание URLSearchParams из строки запроса
-        const urlParams = new URLSearchParams(initData);
-        
-        // Получение JSON строки и парсинг ее
-        const userJson = urlParams.get('user');
-        const user = userJson ? JSON.parse(userJson) : {};
-        
-        // Извлечение userId
-        setUserId(user.id);
+      
+    try{
+        const { initData } = retrieveLaunchParams();
+      // Проверка, что initData определен и имеет корректный формат
+      if (initData) {
+        try {
+          // Создание URLSearchParams из строки запроса
+          const urlParams = new URLSearchParams(initData);
+          
+          // Получение JSON строки и парсинг ее
+          const userJson = urlParams.get('user');
+          const user = userJson ? JSON.parse(userJson) : {};
+          
+          // Извлечение userId
+          setUserId(user.id);
 
-        console.log('User ID:', userId);
-      } catch (error) {
-        console.error('Ошибка при обработке initData:', error);
+          console.log('User ID:', userId);
+        } catch (error) {
+          console.error('Ошибка при обработке initData:', error);
+        }
+      } else {
+        console.error('initData не определен');
       }
-    } else {
-      console.error('initData не определен');
+    }
+    catch(e){
+      console.log(e)
     }
   },[userId])
   
