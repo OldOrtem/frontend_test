f696deb95a15ef3b6863d5bd7f154e2eb84d23d1
ARTEM <artemsmirnof@mail.ru>
2024-08-14 14:50:54 +0300

    fvf

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect } from 'react';
-// import { SDKProvider} from '@telegram-apps/sdk-react';
-// import TappingPage from './components/TappingPage';
+import React from 'react';
+import { SDKProvider} from '@telegram-apps/sdk-react';
+import TappingPage from './components/TappingPage';
 import './App.css'
-import statsService from "./service/StatsService";
 
 const App: React.FC = () => {
   // const [userId, setUserId] = useState<string | null>(null);
   
-  useEffect(()=>{
-    statsService.setCoins(statsService.getCoins()+5);
-    statsService.setEnergy(statsService.getEnergy()-5);
-    statsService.saveStats(Number(5));
-  },[])
+
   return (
-    <>
-    </>
-    // <SDKProvider acceptCustomStyles >
+    <SDKProvider acceptCustomStyles >
       
-    //   <TappingPage/>
-    // </SDKProvider>
+      <TappingPage/>
+    </SDKProvider>
   );
 };
 
