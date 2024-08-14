85b4e89e6ec182641c45481ab489baaaea6d1a1f
ARTEM <artemsmirnof@mail.ru>
2024-08-14 17:25:58 +0300

    t

--- src/components/Energy.tsx
+++ src/components/Energy.tsx
@@ -12,10 +12,11 @@ const Energy = observer(() => {
         </div>
         <div className={styles.energy__bar}>
           <div className={styles.energy__bar_active} style={{width:`${120*energyStore.value/1000}px`}}>
-          </div>
           <div className={styles.energy_val}>
               {energyStore.value}
             </div>
+            </div>
+          
         </div>
       </div>
     )
